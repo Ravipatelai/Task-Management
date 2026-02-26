7992a9dd6e0637a4cffd38aa3546456138c17810
Priyanshu6055 <priyanshu6055@gmail.com>
2026-02-26 13:05:36 +0530

    CORS fixed

--- backend/server.js
+++ backend/server.js
@@ -14,36 +14,9 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Setup CORS *before* security middlewares so OPTIONS requests succeed
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://task-management-njex.vercel.app",
-  process.env.CLIENT_URL,
-];
-
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-
-  if (req.method === "OPTIONS") {
-    return res.status(200).end();
-  }
-  next();
-});
-
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: true,
     credentials: true,
   })
 );
